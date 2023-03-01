0ea895e932abe9dfe7f41d45f2008e89e9d6ad93
akanesasu <megazero56@gmail.com>
2023-03-02 01:07:46 +0900

    230302 0107

--- learn-vue-js-master/vue-todo/src/components/TodoInput.vue
+++ learn-vue-js-master/vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    input
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      //저장 로직 : 개발자도구 Application
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem='';
+    },
+  }
 }
 </script>
 
 <style>
 
-</style>
+</style> 
